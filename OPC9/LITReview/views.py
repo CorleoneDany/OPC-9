80f5cb9dc089802aa21f16496a6ab7d420cce640
Dany Corleone <corleonedany@live.fr>
2021-11-25 00:41:17 +0100

    authentification et création d'utilisateurs ok

--- OPC9/LITReview/views.py
+++ OPC9/LITReview/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
-def login(request):
-    return render(request, 'index.html', {})
+def home(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
 
-def register(response):
+    return render(request, 'index.html', {"form":form})
+
+def register(request):
     form = UserCreationForm()
-    return render(response, 'register.html', {"form":form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'register.html', {"form":form})
 
 def profile(request):
     return render(request, 'profile.html', {})
