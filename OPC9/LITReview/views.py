625f6b86dc55b06897228a2550cc23ddf5b284b9
Dany Corleone <corleonedany@live.fr>
2021-11-29 13:28:32 +0100

    ajout de create ticket and review, utilisation de la methode de la doc pour le flux

--- OPC9/LITReview/views.py
+++ OPC9/LITReview/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.views.generic import TemplateView
+
+from .forms import ReviewForm, TicketForm
 from .models import Ticket, Review, User
+from django.db.models import CharField, Value
+
+from itertools import chain
 
 # Create your views here.
 
@@ -46,7 +52,29 @@ def profile(request):
     return render(request, 'profile.html', {})
 
 
+def create_review(request):
+    form = ReviewForm
+    return render(request, 'create_review.html', {"form": form})
+
+
+def create_ticket(request):
+    form = TicketForm
+    return render(request, 'create_ticket.html', {"form": form})
+
+
 def flux(request):
-    reviews = Review.objects.all
-    tickets = Ticket.objects.all
-    return render(request, 'flux.html', {"reviews": reviews, "tickets": tickets})
+    reviews = get_users_viewable_reviews(request.user)
+    # returns queryset of reviews
+    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
+
+    tickets = get_users_viewable_tickets(request.user)
+    # returns queryset of tickets
+    tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
+
+    # combine and sort the two types of posts
+    posts = sorted(
+        chain(reviews, tickets),
+        key=lambda post: post.time_created,
+        reverse=True
+    )
+    return render(request, 'flux.html', {"posts": posts})
