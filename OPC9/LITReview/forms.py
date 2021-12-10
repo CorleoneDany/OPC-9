da34976342529cb514829ede196190deb8e7a5e9
Dany Corleone <corleonedany@live.fr>
2021-12-10 12:11:20 +0100

    formulaires.py mis a jour pour exclure certains champs

--- OPC9/LITReview/forms.py
+++ OPC9/LITReview/forms.py
@@ -6,13 +6,18 @@ from .models import Review, Ticket, UserFollows
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
-        fields = ['headline', 'rating', 'body']
+        fields = ['headline', 'rating', 'body', 'user']
+        labels = {'headline': 'Titre', 'rating': 'Note', 'body': 'Critique'}
+        exclude = ['user']
 
 
 class TicketForm(ModelForm):
     class Meta:
         model = Ticket
-        fields = ['title', 'description', 'image']
+        fields = ['title', 'description', 'user', 'image']
+        labels = {'title': 'Titre',
+                  'description': 'Description', 'image': 'Couverture'}
+        exclude = ['user']
 
 
 class UserFollows(ModelForm):
